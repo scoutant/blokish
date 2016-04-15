cf553364311ba82f412766bfacd9fa776785934e
coutant <support@scoutant.org>
2016-04-15 10:12:07 +0200

    fix bug #13

--- mobile/src/main/java/org/scoutant/blokish/Help.java
+++ mobile/src/main/java/org/scoutant/blokish/Help.java
@@ -18,7 +18,11 @@ public class Help extends Activity {
 		View v = findViewById(R.id.video);
 		v.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				startActivity( new Intent( Intent.ACTION_VIEW, Uri.parse("vnd.youtube://3Q7ow07uaMw")));
+				try {
+					startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube://3Q7ow07uaMw"))); // if device has Youtube app
+				} catch (Exception e) {
+					startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=3Q7ow07uaMw"))); // if not
+				}
 			}
 		});
 	}
